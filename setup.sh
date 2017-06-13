d1e395ee4d8559daf5aac2cdb5a587e04ccef0ce
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-06-13 21:23:27 +0200

    setup.sh: use option -y for apt-get
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install xypron-keyring --allow-unauthenticated -y
 apt-get update
 apt-get install less locales ssh sudo vim flash-kernel swconfig u-boot-sunxi -y
-apt-get install linux-image-armmp
+apt-get install linux-image-armmp -y
 adduser lamobo --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo lamobo:lamobo | chpasswd
 adduser lamobo sudo
