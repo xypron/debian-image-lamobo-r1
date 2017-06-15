6a2cd0bf0b0c3b0bab634619b2182b607672254d
Heinrich Schuchardt <xypron.glpk@gmx.de>
2017-06-15 05:37:04 +0200

    setup.sh: add additional packages
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 apt-get update
 apt-get install xypron-keyring --allow-unauthenticated -y
 apt-get update
-apt-get install less locales ssh sudo vim flash-kernel swconfig u-boot-sunxi -y
+apt-get install dnsutils flash-kernel less locales net-tools pci-utils ssh \
+  sudo swconfig u-boot-sunxi usbutils vim -y
 apt-get install linux-image-armmp -y
 adduser lamobo --uid 9997 --disabled-password --gecos 'Default User,,,'
 echo lamobo:lamobo | chpasswd
